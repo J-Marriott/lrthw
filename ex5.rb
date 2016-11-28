e9366d1819dfa0b4c5d0a785539365bcf82f4c28
Joe Marriott <marriott90@gmail.com>
2016-11-28 17:05:13 +0000

    added ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,20 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
+
+height_metric = (height*2.54).round(2) #converts to cm and rounds to 2 decimal places
+weight_metric = (weight/2.2).round(2) #converts to kg and rounds to 2 decimal places
