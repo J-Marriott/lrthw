ac33a7ea1e42dfaf4261e12a7a7e275cb8f3377c
Joe Marriott <marriott90@gmail.com>
2016-11-28 15:23:00 +0000

    added ex1.rb

---
+++ ex1.rb
@@ -0,0 +1,15 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+#puts "Hash at beginning of line makes line a comment"
+
+=begin
+
+This is another way to comment
+
+=end
