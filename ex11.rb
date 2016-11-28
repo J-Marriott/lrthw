ca5ea209823800c7565386207c080cbaeaaef3dd
Joe Marriott <marriott90@gmail.com>
2016-11-28 18:19:36 +0000

    added ex11.rb

---
+++ ex11.rb
@@ -0,0 +1,14 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+
+print "What is your favourite number?"
+fave_number = gets.chomp
+
+puts "Your favourite number is #{fave_number}"
