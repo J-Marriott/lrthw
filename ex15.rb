75399e7740d94e19bba7c562dff3033f16287f91
Joe Marriott <marriott90@gmail.com>
2016-11-28 19:37:47 +0000

    added ex15.rb updated comments

--- ex15.rb
+++ ex15.rb
@@ -6,6 +6,8 @@ txt = open(filename)
 puts "Here's your file #{filename}:"
 # prints txt variable (open.filename) and adds .read
 print txt.read
+# Close file object
+txt.close
 # prompts the user to type the filename again
 print "Type the filename again: "
 # points file_again variable to the response of above question
@@ -14,3 +16,5 @@ file_again = $stdin.gets.chomp
 txt_again = open(file_again)
 # prints txt_again variable which has had the read function called on it
 print txt_again.read
+# close the file object
+txt_again.close
