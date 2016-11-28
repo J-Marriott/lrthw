52f078f6507b0080fad9cf819fc207c37e5e4384
Joe Marriott <marriott90@gmail.com>
2016-11-28 17:18:02 +0000

    added ex6.rb

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+# assigning value of 10 to variable types_of_people
+types_of_people = 10
+# assigning a string to variable x that has types_of_people concatenated in
+x = "There are #{types_of_people} types of people."
+# assigning string "binary" to variable binary
+binary = "binary"
+# assigning string "don't" to variable do_not
+do_not = "don't"
+# assigning string to variable y with variables concantenated in
+y = "Those who know #{binary} and those who #{do_not}."
+
+# puts out value of variable x
+puts x
+# puts out value of variable y
+puts y
+# puts with variable x concantenated in
+puts "I said: #{x}."
+# puts with variable y concantenated in between single quotes
+puts "I also said: '#{y}'."
+# assigns boolean value false to varaible hilarious
+hilarious = false
+# assigns a string with variable hilarious concantenated in to new variable joke_evaluation
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# puts varaible joke_evaluation
+puts joke_evaluation
+# assigns strings to variables w and e
+w = "This is the left side of..."
+e = "a string with a right side."
+# puts w + e variables merged
+puts w + e
