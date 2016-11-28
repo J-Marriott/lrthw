91ecc6edacb348f8399e9f46a93d28f7360a955e
Joe Marriott <marriott90@gmail.com>
2016-11-28 17:28:54 +0000

    added ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,20 @@
+puts "Mary had a little lamb." # put string
+puts "It's fleece was white as #{'snow'}." #put string with string concantenated in
+puts "And everywhere that Mary went." # put string
+puts "." * 10 # puts 10 full stops
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6 # prints separate variables spelling 'Cheese'
+puts end7 + end8 + end9 + end10 + end11 + end12 # put variables converted to string at end of 'Cheese' spelling 'Burger'
